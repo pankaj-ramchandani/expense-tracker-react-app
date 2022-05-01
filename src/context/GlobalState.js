c7071a9a204831da87cdd96a5528d3e7f00a47f3
“Pankaj” <ramchandanipankaj787@gmail.com>
2022-05-01 14:40:03 +0530

    working on reducer

---
+++ src/context/GlobalState.js
@@ -0,0 +1,30 @@
+import React, { Children, createContext, useReducer } from "react";
+import AppReducer from './AppReducer';
+
+// inital state
+const initalState = {
+  transactions: [
+    { id: 1, text: "Flower", amount: -20 },
+    { id: 2, text: "Salary", amount: 300 },
+    { id: 3, text: "Book", amount: -10 },
+    { id: 4, text: "Camera", amount: 150 },
+  ],
+};
+
+// Create context
+export const GlobalContext = createContext(initalState);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initalState);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
